01ee3233447a825eb928b7fe8a3198904821b63f
DevHolako <holakonoob@gmail.com>
2023-03-31 22:18:47 +0000

    Db Sedders

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class RoleSeeder extends Seeder
@@ -12,8 +13,12 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        Role::create(['name' => 'Owner', 'guard_name' => 'sanctum']);
-        Role::create(['name' => 'Admin', 'guard_name' => 'sanctum']);
+        $onwer = Role::create(['name' => 'Owner', 'guard_name' => 'sanctum']);
+        $admin = Role::create(['name' => 'Admin', 'guard_name' => 'sanctum']);
         Role::create(['name' => 'User', 'guard_name' => 'sanctum']);
+
+        $permissions = Permission::all();
+        $onwer->syncPermissions($permissions);
+        $admin->syncPermissions($permissions);
     }
 }
