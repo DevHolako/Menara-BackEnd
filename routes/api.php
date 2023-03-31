d104e290830439b257315967513d3dc7b8607c8c
DevHolako <holakonoob@gmail.com>
2023-03-31 22:18:57 +0000

    Config new Route system

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\ArticleController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CategorieController;
+use App\Http\Controllers\Api\ClientController;
 use App\Http\Controllers\Api\PermissionController;
 use App\Http\Controllers\Api\RoleController;
 use App\Http\Controllers\Api\UserController;
@@ -37,16 +40,31 @@ Route::get('reset-password/{token}', function ($token) {
 // ------------ Protected Routes ------------ //
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
-
     // Auth-related routes
     Route::post("/logout", [AuthController::class, "logout"]);
+
+    // User-related routes
+    Route::apiResource('users', UserController::class);
+    Route::post('/user/{user}/restore', [UserController::class, 'restore']);
+    Route::delete('/user/{user}/force_delete', [UserController::class, 'forceDelete']);
+
+    // Client-related routes
+    Route::apiResource('clients', ClientController::class);
+    Route::post('/clients/{client}/restore', [ClientController::class, 'restore']);
+    Route::delete('/clients/{client}/force_delete', [ClientController::class, 'forceDelete']);
+
+    // Article-related routes
+    Route::apiResource('articles', ArticleController::class);
+    Route::post('/articles/article/restore', [ArticleController::class, 'restore']);
+    Route::delete('/articles/article/force_delete', [ArticleController::class, 'forceDelete']);
+
+    // Categorie-related routes
+    Route::apiResource('categories', CategorieController::class);
+    Route::post('/categories/categorie/restore', [CategorieController::class, 'restore']);
+    Route::delete('/categories/categorie/force_delete', [CategorieController::class, 'forceDelete']);
+
     // Admin Routes
     Route::middleware(['role:Owner|Admin'])->group(function () {
-        // User-related routes
-        Route::post('/user/{user}/restore', [UserController::class, 'restore']);
-        Route::delete('/user/{user}/force_delete', [UserController::class, 'forceDelete']);
-        Route::apiResource('users', UserController::class);
-
         // roles and permes routes
         Route::apiResource('/permissions', PermissionController::class);
         Route::apiResource('/roles', RoleController::class);
