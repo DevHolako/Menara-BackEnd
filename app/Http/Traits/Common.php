d94b06145db0169c16ac7fb4cfb64317c15b7dc9
DevHolako <holakonoob@gmail.com>
2023-04-06 21:14:09 +0000

    back-end checking for role

--- app/Http/Traits/Common.php
+++ app/Http/Traits/Common.php
@@ -3,22 +3,32 @@ namespace App\Http\Traits;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 trait Common
 {
     public function CreateUser($req)
     {
         $fileds = $req->validate([
-            'fullname' => "string|required",
-            'username' => "string|required|unique:users,username",
-            'email' => "email|required|unique:users,email",
-            "role" => "string|sometimes",
-            'password' => "string|required|confirmed|min:8",
+            'fullname' => 'string|required',
+            'username' => 'string|required|unique:users,username',
+            'email' => 'email|required|unique:users,email',
+            'role' => 'string|sometimes',
+            'password' => 'string|required|confirmed|min:8',
         ]);
-
         $fileds['password'] = Hash::make($fileds['password']);
+
+        if (!isset($fileds['role'])) {
+            $fileds['role'] = 'User';
+        }
+
+        if (!Role::where('name', $fileds['role'])->exists()) {
+            $fileds['role'] = 'User';
+        }
+
         $user = User::create($fileds);
-        $user->assignRole($fileds->role);
+        $user->assignRole($fileds['role']);
+
         return response()->json(['message' => 'User created successfully', 'user' => $user], 201);
 
     }
