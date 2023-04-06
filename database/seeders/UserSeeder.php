141f1a7d716f2aab7ac7ec46aff413483db3634f
DevHolako <holakonoob@gmail.com>
2023-04-06 14:17:42 +0000

    fullname

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,24 +13,21 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::create([
-            "first_name" => "Holako",
-            "last_name" => "Noob",
+            "fullname" => "Holako",
             "username" => "HolakoNoob",
             "email" => "holako@p.com",
             "password" => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
 
         ])->assignRole('Owner');
         User::create([
-            "first_name" => "admin",
-            "last_name" => "",
+            "fullname" => "admin",
             "username" => "admin",
             "email" => "admin@p.com",
             "password" => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
 
         ])->assignRole('Admin');
         User::create([
-            "first_name" => "Test",
-            "last_name" => "User",
+            "fullname" => "Test",
             "username" => "user",
             "email" => "user@p.com",
             "password" => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
