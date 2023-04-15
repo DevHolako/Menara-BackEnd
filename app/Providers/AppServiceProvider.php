3ad54b9e2ec6529a097c584301723d55caacbe6a
DevHolako <holakonoob@gmail.com>
2023-04-15 00:54:26 +0000

    app

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,6 @@
 
 namespace App\Providers;
 
-use App\Http\Resources\API\Article\AricleResource;
-use App\Http\Resources\API\Categorie\CategorieResource;
-use App\Http\Resources\API\Client\CleintResource;
-use App\Http\Resources\API\Devi\DeviResource;
 use App\Http\Resources\Api\Permission\PermissionResource;
 use App\Http\Resources\Api\Role\RoleResource;
 use App\Http\Resources\Api\User\UserResource;
@@ -30,10 +26,6 @@ class AppServiceProvider extends ServiceProvider
         UserResource::withoutWrapping();
         RoleResource::withoutWrapping();
         PermissionResource::withoutWrapping();
-        DeviResource::withoutWrapping();
-        CleintResource::withoutWrapping();
-        CategorieResource::withoutWrapping();
-        AricleResource::withoutWrapping();
 
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
